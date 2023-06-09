751c7eef363f620e6cba87554e1bcfc34ec9d3bc
Kartik Goel <kartik.2024it1170@kiet.edu>
2023-06-09 21:00:47 +0530

    modified:   components/Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,14 +4,15 @@ import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 function Header() {
     return (
     <header>
-        <div className="flex felx-col md:flex-row items-row items-center p-5 bg-gray-500/10">
+        <div className="flex flex-col md:flex-row items-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
         <Image src="https://links.papareact.com/c2cdd5"
         alt="Trello logo"
         width={300}
         height={100}
         className="w-44 md:-56 pb-10 md:pb-0 object-contain"
         />
-        <div>
+
+        <div className="">
             {/*search box */}
             <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial  ">
                 <MagnifyingGlassIcon className="h-6 w-6  text-gray-400"/>
