39033538fc1a02a97af34c2704cd8f9fd283d046
Kartik Goel <kartik.2024it1170@kiet.edu>
2023-06-10 21:54:45 +0530

    modified:   app/layout.tsx 	modified:   components/Header.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,11 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body className='bg-[#F5F6F8]'>{children}</body>
     </html>
   )
 }
