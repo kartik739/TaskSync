a08bf81cfed1702bf0cf40368170e7c68fcc65c2
Kartik Goel <kartik.2024it1170@kiet.edu>
2023-06-12 23:03:49 +0530

    modified:   components/Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ function Header() {
     return (
     <header>
         <div className="flex flex-col md:flex-row items-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+            <div  className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 t0-[#0055D1] rounded-md filter blue-3xl opacity-50 -z-50"/>
         <Image src="https://links.papareact.com/c2cdd5" alt="Trello logo" width={300} height={100}
         className="w-44 md:-56 pb-10 md:pb-0 object-contain"    />
 
@@ -24,7 +25,7 @@ function Header() {
             <Avatar name="Kartik Goel" round size="50" color="#0055D1"/>
         </div>
         </div>
-        <div className="flex items-center justify-center px-5 md:py-5">
+        <div className="flex items-center justify-center px-5 py-2 md:py-5">
             <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
                 <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1"/>
                 GPT is summarising your tasks for the day......
