f8dcfc909217ba1a3cdcb270ace7c04634ae8ce6
Kartik Goel <kartik.2024it1170@kiet.edu>
2023-06-17 20:12:20 +0530

    modified:   app/page.tsx 	new file:   components/Board.tsx 	modified:   package-lock.json 	modified:   package.json
    	new file:   components/Board.tsx

---
+++ components/Board.tsx
@@ -0,0 +1,14 @@
+import {DragDropContext, Droppable} from 'react-beautiful-dnd';
+
+function Board () {
+  return 
+    <DragDropContext>
+            <head>
+                hello world
+            </head>
+
+    </DragDropContext>
+  
+}
+
+export default Board
