4128212800d72bb8f812247c1472b47d1df0289d
Kartik Goel <kartik.2024it1170@kiet.edu>
2023-07-01 17:23:46 +0530

    update
     Date:      Sat Jul 1 17:23:46 2023 +0530
    	modified:   .vscode/settings.json
    	new file:   appwrite.ts
    	modified:   components/Board.tsx
    	modified:   package-lock.json
    	modified:   package.json

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,6 +1,14 @@
+'use client'
+
+import { useEffect } from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function Board() {
+  useEffect(() => {
+    // getboard();
+
+  },[])
+
   return 
     <DragDropContext>
         <Droppable droppableId="board" direction="horizontal" type="column">
